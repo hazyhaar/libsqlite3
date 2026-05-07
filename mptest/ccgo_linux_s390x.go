856ed8a01d9aac2d6271546a2239b1fe48e7e3bd
Jan Mercl <0xjnml@gmail.com>
2026-05-07 09:38:15 +0200

    modernc.org/libsqlite3: s390x auto generate

--- mptest/ccgo_linux_s390x.go
+++ mptest/ccgo_linux_s390x.go
@@ -1,4 +1,4 @@
-// Code generated for linux/s390x by 'generator -DNDEBUG -I /tmp/libsqlite3/sqlite-src-3530100 -ignore-unsupported-alignment -ignore-link-errors -o mptest/ccgo_linux_s390x.go /tmp/libsqlite3/sqlite-src-3530100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/s390x by 'generator -DNDEBUG -DSQLITE_DISABLE_INTRINSIC -I /tmp/libsqlite3/sqlite-src-3530100 -ignore-unsupported-alignment -ignore-link-errors -o mptest/ccgo_linux_s390x.go /tmp/libsqlite3/sqlite-src-3530100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && s390x
 
