817bec1ef80cf1a96782b1c21711a673293b9ed0
Jan Mercl <0xjnml@gmail.com>
2026-07-06 23:13:13 +0200

    update dependecies

--- generator.go
+++ generator.go
@@ -155,7 +155,7 @@ func main() {
 	ilibtcl := filepath.Join(cwd, "..", "libtcl8.6", "include", goos, goarch)
 	result := "sqlite3.go"
 	util.MustInDir(true, makeRoot, func() (err error) {
-		util.MustShell(true, nil, "sh", "-c", "go mod init example.com/libsqlite3 ; go get modernc.org/libc@v1.73.5 modernc.org/libz@v0.18.0 modernc.org/libtcl8.6@v0.19.0")
+		util.MustShell(true, nil, "sh", "-c", "go mod init example.com/libsqlite3 ; go get modernc.org/libc@v1.74.0 modernc.org/libz@v0.18.0 modernc.org/libtcl8.6@v0.19.0")
 		config := []string{os.Args[0]}
 		if dev {
 			util.MustShell(true, nil, "sh", "-c", "go work init ; go work use $GOPATH/src/modernc.org/libc $GOPATH/src/modernc.org/libz $GOPATH/src/modernc.org/libtcl8.6")
@@ -320,7 +320,7 @@ func main() {
 		util.MustShell(true, nil, "sh", "-c", `
 go mod init example.com/libsqlite3
 go get \
-	modernc.org/libc@v1.73.5 \
+	modernc.org/libc@v1.74.0 \
 	modernc.org/libtcl8.6@v0.19.0 \
 	modernc.org/libz@v0.18.0 \
 `)
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	golang.org/x/sys v0.46.0
 	modernc.org/ccgo/v4 v4.34.6
 	modernc.org/fileutil v1.4.0
-	modernc.org/libc v1.73.5
+	modernc.org/libc v1.74.0
 	modernc.org/libtcl8.6 v0.19.0
 	modernc.org/libz v0.18.0
 )
@@ -22,7 +22,7 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/mod v0.37.0 // indirect
 	golang.org/x/sync v0.21.0 // indirect
-	golang.org/x/tools v0.46.0 // indirect
+	golang.org/x/tools v0.47.0 // indirect
 	modernc.org/cc/v4 v4.29.0 // indirect
 	modernc.org/gc/v2 v2.6.5 // indirect
 	modernc.org/gc/v3 v3.1.4 // indirect
