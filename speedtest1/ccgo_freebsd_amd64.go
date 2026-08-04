4f3ffd51270b128fd6c71d34dcc85da7e81c40f7
Jan Mercl <0xjnml@gmail.com>
2026-08-04 04:37:30 +0200

    modernc.org/libsqlite3: freebsd64 auto generate

--- speedtest1/ccgo_freebsd_amd64.go
+++ speedtest1/ccgo_freebsd_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for freebsd/amd64 by 'generator -DNDEBUG -DSQLITE_DISABLE_INTRINSIC -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_freebsd_amd64.go -I /tmp/libsqlite3/sqlite-src-3530300 /tmp/libsqlite3/sqlite-src-3530300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for freebsd/amd64 by 'generator -DNDEBUG -DSQLITE_DISABLE_INTRINSIC -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_freebsd_amd64.go -I /tmp/libsqlite3/sqlite-src-3530400 /tmp/libsqlite3/sqlite-src-3530400/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build freebsd && amd64
 
